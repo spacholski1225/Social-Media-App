68b2e7d2c7b3f8370c78ba98fea83c8d9a473ff5
spacholski1225 <spacholski1225@gmail.com>
2021-05-26 18:20:41 +0200

    created temporarily user repository

---
+++ SocialMedia/Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User(int id, string name, string password)
+        {
+            (Id, Name, Password) = (id, name, password);
+        }
+    }
+}
