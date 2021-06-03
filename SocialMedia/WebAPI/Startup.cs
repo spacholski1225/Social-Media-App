5f3c9ad6b76ac00d0a82717d5ae317f19b849ed7
spacholski1225 <spacholski1225@gmail.com>
2021-06-03 11:00:05 +0200

    Adden filter to swager

--- SocialMedia/WebAPI/Startup.cs
+++ SocialMedia/WebAPI/Startup.cs
@@ -19,6 +19,9 @@ using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 using Infrastructure.Services;
+using Application.Filter;
+using System.IO;
+using System.Reflection;
 
 namespace WebAPI
 {
@@ -37,31 +40,15 @@ namespace WebAPI
 
             services.AddControllers();
             services.AddDbContext<DatabaseConfig>(opt =>
-            opt.UseSqlServer(Configuration.GetConnectionString("SqlConnection"), b => b.MigrationsAssembly("WebAPI")));
-            services.AddIdentity<IdentityUser, IdentityRole>() // change to opt.SignIn.RequireConfirmedAccount = true
-                .AddEntityFrameworkStores<DatabaseConfig>();
+                opt.UseSqlServer(Configuration.GetConnectionString("SqlConnection"), b => b.MigrationsAssembly("WebAPI")));
+
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<AutoMapperConfig>();
-            services.AddScoped<IIdentityService, IdentityService>();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-                var security = new Dictionary<string, IEnumerable<string>>
-                {
-                    {"Bearer", new string[0]}
-                };
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the bearer scheme",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-            });
 
             var jwtSettings = new JwtSettings();
             Configuration.Bind(nameof(jwtSettings), jwtSettings);
             services.AddSingleton(jwtSettings);
+            services.AddScoped<IIdentityService, IdentityService>();
 
             services.AddAuthentication(x =>
             {
@@ -71,17 +58,42 @@ namespace WebAPI
             })
                 .AddJwtBearer(x =>
                 {
+                    var key = Encoding.ASCII.GetBytes(Configuration["JwtSettings:Secret"]);
                     x.SaveToken = true;
                     x.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         RequireExpirationTime = true,
                         ValidateLifetime = true
                     };
                 });
+            services.AddIdentity<IdentityUser, IdentityRole>(opt => opt.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<DatabaseConfig>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[0] }
+                };
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT"
+                });
+                c.OperationFilter<AuthOperationFilter>();
+               
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
