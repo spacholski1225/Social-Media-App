ce8c941b052d1c2ba3d36dd7c2640de6df6895ab
spacholski1225 <spacholski1225@gmail.com>
2021-06-08 17:03:33 +0200

    move profile repository into application section and created profile dto

---
+++ SocialMedia/Application/Dto/ProfileDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProfileDto //have to extent identity user class with some properties like first last name, city, image etc.
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    
+    }
+}
