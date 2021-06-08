1261865049f82824899a4876c83ffcb79b062fd0
spacholski1225 <spacholski1225@gmail.com>
2021-06-08 17:18:14 +0200

    fixed architecture

---
+++ SocialMedia/Infrastructure/Repositories/ProfileRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Config;
+using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        private readonly DatabaseConfig
+        public ProfileRepository()
+        {
+                
+        }
+        public Task<ProfileDto> GetUserProfile(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
