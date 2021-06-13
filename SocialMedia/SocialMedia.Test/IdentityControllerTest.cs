39e4a8636c35b5f44c7bb5709012fde95c68a227
spacholski1225 <spacholski1225@gmail.com>
2021-06-13 13:06:23 +0200

    Created test for initial controller method register checkin if returned result if type authentication result and is not null

--- SocialMedia/SocialMedia.Test/IdentityControllerTest.cs
+++ SocialMedia/SocialMedia.Test/IdentityControllerTest.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Application.Requests;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAPI.Controllers;
+using Xunit;
 
 namespace SocialMedia.Test
 {
-    class IdentityControllerTest
+    public class IdentityControllerTest
     {
+        private readonly Mock<IIdentityService> _identityServiceMock;
+        private readonly IdentityController _controller;
+        public IdentityControllerTest()
+        {
+            _identityServiceMock = new Mock<IIdentityService>();
+            _controller = new IdentityController(_identityServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task Register_ReturnSuccessResult()
+        {
+            //Arrange
+            _identityServiceMock.Setup(s => s.RegisterAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new AuthenticationResult());
+            //Act
+            var result = await _controller.Register(new UserRegistrationRequest());
+            //Assert
+            Assert.IsType<AuthenticationResult>(result);
+            Assert.NotNull(result);
+        }
     }
 }
