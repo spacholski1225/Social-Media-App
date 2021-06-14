caae172ee9f30a07a9f27504ae6a993824f7a145
spacholski1225 <spacholski1225@gmail.com>
2021-06-14 18:11:27 +0200

    Created test for Profile Controller for Get Profile method whos check if returned type is BadRequest or OkResult for null or not null profile

--- SocialMedia/SocialMedia.Test/ProfileControllerTest.cs
+++ SocialMedia/SocialMedia.Test/ProfileControllerTest.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAPI.Controllers;
+using Xunit;
 
 namespace SocialMedia.Test
 {
-    class ProfileControllerTest
+    public class ProfileControllerTest
     {
+        private readonly ProfileController _controller;
+        private readonly Mock<IProfileRepository> _mockProfileRepository;
+
+        public ProfileControllerTest()
+        {
+            _mockProfileRepository = new Mock<IProfileRepository>();
+            _controller = new ProfileController(_mockProfileRepository.Object);
+        }
+        
+        [Fact]
+        public async Task GetProfile_ReturnOkResult_WhenProfileIsNotNull()
+        {
+            //Arrange
+            _mockProfileRepository.Setup(s => s.GetUserProfileAsync(It.IsAny<string>())).ReturnsAsync(new ProfileDto());
+            //Act
+            var result = await _controller.GetProfile("test");
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetProfile_ReturnBadResult_WhenProfileIsNull()
+        {
+            //Arrange
+            _mockProfileRepository.Setup(s => s.GetUserProfileAsync(It.IsAny<string>())).ReturnsAsync(() => null);
+            //Act
+            var result = await _controller.GetProfile("test");
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
     }
 }
