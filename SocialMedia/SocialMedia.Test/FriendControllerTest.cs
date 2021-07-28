ad61af2177007c10d255ffab5ce5c1d49044fb3d
spacholski1225 <spacholski1225@gmail.com>
2021-07-28 10:58:26 +0200

    created ctor in test class

--- SocialMedia/SocialMedia.Test/FriendControllerTest.cs
+++ SocialMedia/SocialMedia.Test/FriendControllerTest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAPI.Controllers;
 
 namespace SocialMedia.Test
 {
     public class FriendControllerTest
     {
+        private readonly FriendController _controller;
+        public FriendControllerTest()
+        {
+
+        }
+       
     }
 }
