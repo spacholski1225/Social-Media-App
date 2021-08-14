a4ea18189125b075ad73f2a57f3629b1458c0687
spacholski1225 <spacholski1225@gmail.com>
2021-08-14 18:32:39 +0200

    Created unit tests for GetListOfFriends from FriendController. Tests checks if returned type is BadRequest or Ok when friend list is empty or not empty

--- SocialMedia/SocialMedia.Test/FriendControllerTest.cs
+++ SocialMedia/SocialMedia.Test/FriendControllerTest.cs
@@ -1,19 +1,49 @@
-﻿using System;
+﻿using Application.Requests.Friend;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPI.Controllers;
+using Xunit;
 
 namespace SocialMedia.Test
 {
     public class FriendControllerTest
     {
         private readonly FriendController _controller;
+        private readonly Mock<IFriendRepository> _mockFriendRepository;
         public FriendControllerTest()
         {
+            _mockFriendRepository = new Mock<IFriendRepository>();
 
+            _controller = new FriendController(_mockFriendRepository.Object);
         }
-       
+        [Fact]
+        public void GetListOfFriends_ReturnBadRequest_WhenFriendListIsEmpty()
+        {
+            //Arrange
+            _mockFriendRepository.Setup(s => s.GetAllFriends(It.IsAny<string>())).Returns(new List<Friend>());
+            //Act
+            var result = _controller.GetListOfFriends(new GetUserIdRequest());
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+        [Fact]
+        public void GetListOfFriends_ReturnOkResult_WhenFriendListIsNotEmpty()
+        {
+            //Arrange
+            _mockFriendRepository.Setup(s => s.GetAllFriends(It.IsAny<string>())).Returns(new List<Friend>
+            { new Friend() });
+            //Act
+            var result = _controller.GetListOfFriends(new GetUserIdRequest());
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
     }
 }
